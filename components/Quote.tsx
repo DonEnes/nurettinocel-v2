c1888c9c49bc387b49519497850def030cbbcc5a
DonEnes <ocelnurettinalperen@gmail.com>
2024-12-17 01:43:04 +0100

    fix finish

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -13,12 +13,12 @@ export const Quote = () => {
             <div className="absolute inset-0 bg-black bg-opacity-60" />
             <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
                 <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} className="space-y-8">
-                    <span className="text-gray-200 text-xs md:text-sm font-semibold bg-clip-text">NURETTIN ÖCEL | WEBDESIGNER & ENTWICKLER</span>
+                    <span className="text-gray-300 text-xs md:text-xs font-bold bg-clip-text">NURETTIN ÖCEL | WEBDESIGNER & ENTWICKLER</span>
                     <motion.h1
                         initial={{ opacity: 0, y: 20 }}
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.8, delay: 0.3 }}
-                        className="text-1xl md:text-3xl text-white">
+                        className="text-2xl md:text-3xl text-white">
                         &quot;Mit mir setzt du auf ein Webdesigner mit Full-Service-Konzept, der kleine und mittelständische Unternehmen
                         voranbringt.&quot;
                         <br />
